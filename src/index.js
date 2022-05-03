093878fc7c32a7b02ce107f6e950192761e3321e
Doug Bostick <dougbostick@Dougs-MacBook.lan>
2022-05-03 18:30:59 -0400

    working on hint

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ class App extends React.Component {
       pastGuesses: [],
       winningNumber: null, //generateWinningNumber();
       status: "",
+      hint: [],
     };
     this.checkGuess = this.checkGuess.bind(this);
     this.playersGuessSubmission = this.playersGuessSubmission.bind(this);
@@ -34,7 +35,7 @@ class App extends React.Component {
       arr[m] = arr[i];
       arr[i] = t;
     }
-    return arr;
+    return this.setState({ hint: arr });
   }
 
   difference() {
@@ -54,6 +55,7 @@ class App extends React.Component {
     console.log("num", num);
     console.log("typeof", typeof num);
     if (num < 1 || num > 100 || isNaN(num)) {
+      this.setState({ status: "Must guess a number between 1-100" });
       throw "That is an invalid guess.";
     }
     // this.setState({ pastGuesses: [...this.state.pastGuesses, num] });
@@ -62,66 +64,158 @@ class App extends React.Component {
     return this.checkGuess();
   }
 
-  checkGuess() {
-    console.log("checkguess diff", this.difference());
-    let dif = this.difference();
-    console.log("dif", dif);
-
+  checkStatus() {
     if (this.state.playersGuess === this.state.winningNumber) {
-      return this.setState({ status: "You Win!" });
+      return "You Win!";
     }
-    if (this.state.pastGuesses.includes(this.playersGuess)) {
-      return this.setState({ status: "You have already guessed that number." });
-    } else {
-      this.setState({
-        pastGuesses: [...this.state.pastGuesses, this.state.playersGuess],
-      });
-      if (this.state.pastGuesses.length > 4) {
-        return this.setState({ status: "You Lose!" });
-      }
-    }
-
     if (this.difference() < 10) {
-      return this.setState({ status: "You're burning up!" });
+      return "You're burning up!";
     }
     if (this.difference() < 25) {
-      return this.setState({ status: "You're lukewarm." });
+      return "You're lukewarm.";
     }
     if (this.difference() < 50) {
-      return this.setState({ status: "You're a bit chilly." });
+      return "You're a bit chilly.";
     }
     if (this.difference() < 100) {
-      return this.setState({ status: "You're ice cold!" });
+      return "You're ice cold!";
     }
-    console.log("state after check", this.state);
+  }
+
+  checkGuess() {
+    const status = this.checkStatus();
+    if (this.state.pastGuesses.includes(this.state.playersGuess)) {
+      return this.setState({ status: "You have already guessed that number." });
+    } else {
+      const newState = [...this.state.pastGuesses, this.state.playersGuess];
+      return this.setState({
+        pastGuesses: newState,
+        status:
+          newState.length > 4 && !newState.includes(this.state.winningNumber)
+            ? "You Lose!"
+            : status,
+      });
+    }
   }
 
   provideHint() {
     let hint = [];
-    hint.push(this.winningNumber);
+    hint.push(this.state.winningNumber);
     while (hint.length < 3) {
-      hint.push(generateWinningNumber());
+      const hintNum = Math.floor(Math.random() * 100) + 1;
+      hint.push(hintNum);
     }
-    return shuffle(hint);
+    console.log("hint", hint);
+
+    return this.shuffle(hint);
   }
+
+  newGame() {
+    this.setState({
+      playersGuess: null,
+      pastGuesses: [],
+      winningNumber: null,
+      status: "",
+      hint: [],
+    });
+    this.generateWinningNumber();
+  }
+  clear() {
+    document.querySelector("input").value = "";
+  }
+
   render() {
     console.log("state", this.state);
+    const hintDiv = this.state.pastGuesses.includes(this.state.winningNumber)
+      ? null
+      : this.state.hint.map((hint) => {
+          return <div key={hint}> {hint}</div>;
+        });
+    const prevGuesses = this.state.pastGuesses.map((guess) => {
+      return (
+        <div key={guess} className="guess">
+          {guess}
+        </div>
+      );
+    });
+    const instructions = this.state.pastGuesses.length ? null : (
+      <p>
+        Guess a number between 1-100, <br></br> you only have 5 chances!{" "}
+        <br></br> Good luck!
+      </p>
+    );
+    const hintInstructions =
+      this.state.pastGuesses.includes(this.state.winningNumber) ||
+      this.state.pastGuesses.length < 3 ||
+      this.state.pastGuesses.length > 4 ? null : (
+        <p>You can ask for a hint!</p>
+      );
+
     return (
-      <div>
+      <div className="main">
         <h1>My Guessing Game</h1>
-        <h3>Previous Guesses</h3>
-        <form onSubmit={this.playersGuessSubmission}>
+        <div className="hint">{hintDiv}</div>
+        <h3>Previous Guesses: </h3>
+        <div className="guess_div">{prevGuesses}</div>
+
+        <form onSubmit={this.playersGuessSubmission} className="form">
           <input
             placeholder="input guess"
             onChange={(ev) =>
               this.setState({ playersGuess: parseInt(ev.target.value) })
             }
           />
-          <button type="submit">Submit Guess</button>
-          <button>Play Again</button>
-          <button>Hint</button>
+
+          <button
+            type="submit"
+            disabled={
+              this.state.pastGuesses.length > 4 ||
+              this.state.pastGuesses.includes(this.state.winningNumber)
+            }
+            className="submit"
+            value={this.state.playersGuess}
+            onClick={() => this.clear()}
+          >
+            Submit Guess
+          </button>
         </form>
-        <div>{this.state.status}</div>
+        <div className="buttons">
+          <button
+            className={
+              !this.state.pastGuesses.includes(this.state.winningNumber) &&
+              this.state.pastGuesses.length < 5
+                ? "disabled"
+                : null
+            }
+            onClick={() => this.newGame()}
+            disabled={
+              !this.state.pastGuesses.includes(this.state.winningNumber) &&
+              this.state.pastGuesses.length < 5
+            }
+          >
+            Play Again
+          </button>
+          <button
+            className={
+              this.state.pastGuesses.length > 4 ||
+              this.state.pastGuesses.includes(this.state.winningNumber) ||
+              this.state.pastGuesses.length < 3
+                ? "disabled"
+                : null
+            }
+            disabled={
+              this.state.pastGuesses.length > 4 ||
+              this.state.pastGuesses.includes(this.state.winningNumber) ||
+              this.state.pastGuesses.length < 3
+            }
+            onClick={() => this.provideHint()}
+          >
+            Hint
+          </button>
+          <div className="status">{this.state.status}</div>
+        </div>
+        <div className="instructions">{instructions}</div>
+        <div className="hint_instructions">{hintInstructions}</div>
       </div>
     );
   }
